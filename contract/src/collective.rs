d00ba222c6743c8791b43988e8719653e9a50f1c
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2022-09-16 12:57:16 +0100

    update collectives

--- contract/src/collective.rs
+++ contract/src/collective.rs
@@ -1,17 +1,40 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{AccountId, Balance};
 
+pub type CollectiveId = String;
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub enum CollectiveType {
+    USER,
+    FUND,
+    EVENT,
+    PROJECT,
+    COLLECTIVE,
+    ORGANIZATION,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
 pub struct Collective {
     pub token_id: AccountId,
-    pub collective_type: String,
+    pub collective_balance: Balance,
+    pub collective_type: CollectiveType,
     pub collective_creator: AccountId,
+    pub metadata_hash: String,
 }
 
 impl Collective {
-    pub fn new() {}
-
-    pub fn get_collective(&self, _collective_id: String) {}
-
-    pub fn get_collective_balance(&self, _collective_id: String) -> Balance {
-        0
+    pub fn new(
+        token_id: AccountId,
+        collective_type: CollectiveType,
+        collective_creator: AccountId,
+        metadata_hash: String,
+    ) -> Self {
+        Self {
+            token_id,
+            collective_type,
+            collective_creator,
+            metadata_hash,
+            collective_balance: 0,
+        }
     }
 }
